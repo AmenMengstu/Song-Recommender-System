2ba2c6170a8bc4d7553f2a37b8f5581757ea62f9
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-12-01 15:43:35 +0300

    Create Building Recommender System using Cosine Similarity.py

---
+++ Building Recommender System using Cosine Similarity.py
@@ -0,0 +1,24 @@
+# Create a pandas series with song titles as indices and indices as series values 
+indices = pd.Series(df.index, index=df['song_title']).drop_duplicates()
+
+# Create cosine similarity matrix based on given matrix
+cosine = cosine_similarity(normalized_df)
+
+def generate_recommendation(song_title, model_type=cosine ):
+    """
+    Purpose: Function for song recommendations 
+    Inputs: song title and type of similarity model
+    Output: Pandas series of recommended songs
+    """
+    # Get song indices
+    index=indices[song_title]
+    # Get list of songs for given songs
+    score=list(enumerate(model_type[indices['Parallel Lines']]))
+    # Sort the most similar songs
+    similarity_score = sorted(score,key = lambda x:x[1],reverse = True)
+    # Select the top-10 recommend songs
+    similarity_score = similarity_score[1:11]
+    top_songs_index = [i[0] for i in similarity_score]
+    # Top 10 recommende songs
+    top_songs=df['song_title'].iloc[top_songs_index]
+    return top_songs
